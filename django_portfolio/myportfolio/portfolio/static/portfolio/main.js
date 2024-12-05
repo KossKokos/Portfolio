8e87a1284dac5475a39abc78c2b7c5d9fb2843b3
KossKokos <koskokos0@gmail.com>
2024-12-05 21:04:24 +0000

    "Updated CSS, JavaScript, and HTML files in the portfolio app, including changes to layout, styling, and navigation. Added new templates and views, and modified existing ones."

--- django_portfolio/myportfolio/portfolio/static/portfolio/main.js
+++ django_portfolio/myportfolio/portfolio/static/portfolio/main.js
@@ -1,30 +1,47 @@
-
-const test_h1 = document.querySelector('h1');
-console.log(test_h1)
-
+const test_h1 = document.querySelector("h1");
 
 // document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('.header');
-    const nav = document.querySelector('.navigation');
-    const navHeight = nav.getBoundingClientRect().height;
+const header = document.querySelector(".header");
+const nav = document.querySelector(".navigation");
+const navHeight = nav.getBoundingClientRect().height;
+const showImageBtns = document.querySelectorAll('.show-image');
 
 
-    const stickyNav = function (entries) {
-      const [entry] = entries;
-      if (!entry.isIntersecting) nav.classList.add('sticky');
-      else nav.classList.remove('sticky');
-    };
-  
-    const headerObserver = new IntersectionObserver(stickyNav, {
-      root: null,
-      threshold: 0,
-      rootMargin: `-${navHeight}px`,
-    });
-  
-    headerObserver.observe(header);
-  // });
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
 
-    if (test_h1) {
-      test_h1.style.marginTop = `${navHeight}px`;
-    };
-  
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+// });
+
+if (test_h1) {
+  const test1Marging = 0.0625 * navHeight;
+  test_h1.style.marginTop = `${test1Marging}rem`;
+}
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(function (btn) {
+  btn.addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+
+showImageBtns.addEventListener()
--- django_portfolio/myportfolio/portfolio/views.py
+++ django_portfolio/myportfolio/portfolio/views.py
@@ -5,7 +5,40 @@ from django.template import loader
 from .models import Email
 
 
-# Create your views here.
+img_examples = [
+    {
+        'title': 'Recurent Neural Network',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421788/blstm_training_uitdwd.png"
+    },
+    {
+        'title': 'Linear Regression',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421788/ridge_ibbdxz.png",
+    },
+    {
+        'title': 'Clustering',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421788/clustering_aapydv.png",
+    },
+    {
+        'title': 'SQL',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421788/sql_ntj4f4.png",
+    },
+    {
+        'title': 'Deep Learning',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421788/comparing_models_oqgrh7.png",
+    },
+    {
+        'title': 'Classification',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421789/comparing_results_kcnu15.png",
+    },
+    {
+        'title': 'Data Analysis',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421789/lantitude_dz7phy.png",
+    },
+    {
+        'title': 'Visualization',
+        'url': "https://res.cloudinary.com/dtg29idor/image/upload/v1733421789/corr_matrix_jekicv.png"
+    },
+]
 
 def get_html_file(name):
     template = loader.get_template(name)
@@ -17,7 +50,7 @@ def index(request):
     # output = ', '.join([e.message for e in email_list])
     # return HttpResponse("Hello, world. You're at the polls index.")
     template = get_html_file('portfolio/home.html')
-    data = {'full_name': 'Elon Musk'}
+    data = {'examples': img_examples}
     return HttpResponse(template.render(data, request))
     
 
